6f1b9cb16ce6855930fdc4b8321b25576312e69a
Shawn Rupert <truper63@gmail.com>
2024-02-05 14:31:24 -0500

    2.22 Outputting posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,19 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -19,6 +26,7 @@ import { HeaderComponent } from './header/header.component';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [provideAnimationsAsync('noop')],
   bootstrap: [AppComponent],
