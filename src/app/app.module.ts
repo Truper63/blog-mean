ad8d9bb4e9c495687a217cabcbcb666b31397af7
Shawn Rupert <truper63@gmail.com>
2024-02-05 13:37:14 -0500

    2.20 Installing Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  providers: [provideAnimationsAsync('noop')],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
