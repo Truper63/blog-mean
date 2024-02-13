668313f316b2a9d9b9e9b3aefbfb6a71a2110361
Shawn Rupert <truper63@gmail.com>
2024-02-13 12:56:08 -0500

    3.38 Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello World!");
+app.use("/api/v1/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "akjheohjflakuj",
+      title: "Hello World!",
+      post: "This is a sample post.",
+    },
+    {
+      id: "hfuadualdual",
+      title: "Hello again World!",
+      post: "This is another sample post.",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
