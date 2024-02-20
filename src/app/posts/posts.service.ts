23c7915ad62b48f403c9e236cdd67c3d9ebe8dcc
Shawn Rupert <truper63@gmail.com>
2024-02-20 14:57:38 -0500

    3.42 Adding Angular

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -33,7 +33,12 @@ export class PostsService {
       title,
       content,
     };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/v1/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
