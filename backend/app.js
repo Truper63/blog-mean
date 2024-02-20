5de812f2c99918312ef5e190e1bb666f37a5d53d
Shawn Rupert <truper63@gmail.com>
2024-02-20 14:23:43 -0500

    Revert "Understanding CORS"
    This reverts commit 13e2102e7be9c6aca28fb6d6674525f51c4374b8.

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
-
-app.use(cors());
 
 app.use("/api/v1/posts", (req, res, next) => {
   const posts = [
     {
       id: "akjheohjflakuj",
       title: "Hello World!",
-      content: "This is a sample post.",
+      post: "This is a sample post.",
     },
     {
       id: "hfuadualdual",
       title: "Hello again World!",
-      content: "This is another sample post.",
+      post: "This is another sample post.",
     },
   ];
   res.status(200).json({
