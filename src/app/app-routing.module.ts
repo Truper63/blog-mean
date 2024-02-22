560f4e109b8e9b1fb5047b6876520a761dddbfa0
Shawn Rupert <truper63@gmail.com>
2024-02-22 15:51:28 -0500

    Fix self created error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostEditComponent } from './posts/post-edit/post-edit.component';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
